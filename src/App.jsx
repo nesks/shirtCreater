eb0e0e0db9dd23faadce24b74ea9bfc9fcf0c9f8
Fabio <fabioguieiro@gmail.com>
2025-03-17 13:46:02 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Canvas from "./canvas";
+import Home from "./pages/Home";
+import Costumizer from "./pages/Costumizer";
+
+function App() {
+  return (
+<main className="app transition-allease-in">
+  <Home/>
+  <Canvas/>
+  <Costumizer/>
+  </main>
+  )
+}
+
+export default App
