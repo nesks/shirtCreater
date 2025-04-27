d146a5ecfa32f0af3edaeee709f2cb0e83011197
Felipe <dev.felipesnunes@gmail.com>
2025-04-27 19:42:46 -0300

    adicionando conta-gotas

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -2,19 +2,50 @@ import React from 'react'
 import {SketchPicker} from 'react-color'
 import { useSnapshot } from 'valtio'
 import state from '../store'
+import { contaGotas } from "../assets";
+
+import CustomButton from './CustomButton'
 
 
 const ColorPicker = () => {
 
   const snap = useSnapshot(state);
 
+  const handleEyedropper = async () => {
+    if (!window.EyeDropper) {
+      alert("Seu navegador não suporta o conta-gotas.");
+      return;
+    }
+
+    try {
+      const eyeDropper = new window.EyeDropper();
+      const result = await eyeDropper.open();
+      state.color = result.sRGBHex; // Atualiza o estado com a cor escolhida
+    } catch (error) {
+      console.error("Erro ao usar o conta-gotas:", error);
+    }
+  };
+
   return (
     <div className='absolute left-full ml-3'>
-      <SketchPicker
-        color={snap.color}
-        disableAlpha
-        onChange={(color) => state.color = color.hex}
-      />
+<div className="relative">
+  <SketchPicker
+    color={snap.color}
+    disableAlpha
+    onChange={(color) => (state.color = color.hex)}
+  />
+
+  <div className="absolute bottom-1 right-1/12 transform -translate-x-1/2">
+    <CustomButton 
+      type="filled"
+      title={<img src={contaGotas} alt="Conta Gotas" className="w-4 h-4" />}
+      handleClick={handleEyedropper}
+      customStyles="text-xs"
+    />
+  </div>
+</div>
+
+
 
     </div>
   )
